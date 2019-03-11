23da6447139ac56c40d2467e38055a639c10ce63
discocisco <f.andresort@gmail.com>
2019-03-11 18:29:32 -0400

    Add success and error messaging for showMovie

--- src/main/movies/messages.js
+++ src/main/movies/messages.js
@@ -1,4 +1,6 @@
 export default {
   getMoviesSuccess: 'Successfully loaded movies',
-  getMoviesFailure: 'Couldn\'t get movies :('
+  getMoviesFailure: 'Couldn\'t get movies :(',
+  showMovieSuccess: 'Successfully loaded movie',
+  showMovieFailure: 'Error on movie load!'
 }
