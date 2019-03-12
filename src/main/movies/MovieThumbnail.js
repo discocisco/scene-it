5e7b837a436ca520943b2c5f4507d521881e4a18
discocisco <f.andresort@gmail.com>
2019-03-12 15:58:11 -0400

    Add link to movie page from MovieThumbnail on home

--- src/main/movies/MovieThumbnail.js
+++ src/main/movies/MovieThumbnail.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const MovieThumbnail = ({ id, name, poster }) => (
   <div className='home-img-thumbnail'>
-    <img src={poster} className='img-thumbnail' />
+    <Link to={'/movies/' + id}>
+      <img src={poster} className='img-thumbnail' />
+    </Link>
   </div>
 )
 
