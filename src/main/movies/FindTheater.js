b567545ef12fa07f37c2c9164ebdc4e2f252b318
discocisco <f.andresort@gmail.com>
2019-03-14 10:56:45 -0400

    Add FindTheater component
    - Communicate to backend to search for movie theaters based on location
      from Yelp's API

---
+++ src/main/movies/FindTheater.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+import { findTheaters } from './api.js'
+
+import YelpCard from './YelpCard.js'
+
+class FindTheater extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      search: '',
+      businesses: []
+    }
+  }
+
+  handleChange = event => this.setState({
+    [event.target.name]: event.target.value
+  })
+
+  submitSearch = event => {
+    event.preventDefault()
+    findTheaters(this.state.search)
+      .then(res => this.setState({ businesses: res.data.businesses }))
+      .catch(console.error)
+  }
+
+  render () {
+    return (
+      <div className='yelp-container'>
+        <form className='find-theater-form' onSubmit={this.submitSearch}>
+          <input
+            required
+            name='search'
+            value={this.state.search}
+            type='text'
+            placeholder='Enter a location'
+            onChange={this.handleChange} />
+          <button type='submit'>Search</button>
+        </form>
+        <div className='found-theaters'>
+          {this.state.businesses.map(theater =>
+            <YelpCard key={theater.id}
+              name={theater.name}
+              imageUrl={theater.image_url}
+              isClosed={theater.is_closed}
+              url={theater.url}
+              reviewCount={theater.review_count}
+              rating={theater.rating.toString().replace(/\./g, '')}
+              displayAddress={theater.location.display_address}
+              displayPhone={theater.display_phone}
+            />)}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withRouter(FindTheater)
